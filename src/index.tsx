ceddb93f1f777b5b1825aa68f4435bdfc01b7251
azagatti <andre.zagatti1@gmail.com>
2020-07-30 16:18:58 -0300

    feat: add react-snap to first html load

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { render, hydrate } from 'react-dom';
 
 import App from './App';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement,
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement,
+  );
+}
