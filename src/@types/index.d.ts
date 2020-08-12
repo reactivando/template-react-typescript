06dc4d8101206795f6dcc12c9da660a4e72dd8d1
azagatti <andre.zagatti1@gmail.com>
2020-08-12 09:38:28 -0300

    refactor: change loader to babel

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 // import '@testing-library/jest-dom/extend-expect';
 
-declare module '*.png' {
-  const value: any;
-  export = value;
-}
+declare module '*.png';
+
+declare module '*.svg';
